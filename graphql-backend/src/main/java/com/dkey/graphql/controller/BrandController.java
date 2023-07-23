de9618010109ff7aee7620cb0c470601fd432219
dkeyceo <dkeyceo@gmail.com>
2023-07-23 21:40:00 +0300

    Added services and controllers for model and described graphql rules for model

--- graphql-backend/src/main/java/com/dkey/graphql/controller/BrandController.java
+++ graphql-backend/src/main/java/com/dkey/graphql/controller/BrandController.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import com.dkey.graphql.entity.Brand;
+import com.dkey.graphql.entity.Model;
+import com.dkey.graphql.enums.Country;
 import com.dkey.graphql.service.BrandService;
 
 @Controller
@@ -23,5 +26,47 @@ public class BrandController {
 	@QueryMapping
 	public Brand findOneBrand(@Argument int id){
 		return brandService.findOneBrand(id);
+	}
+	
+	@MutationMapping
+	public Brand saveBrand(@Argument String name, @Argument Country country){
+		return brandService.saveBrand(name, country);
+	}
+	
+	@MutationMapping
+	public Brand updateBrand(@Argument int id, @Argument String name, @Argument Country country){
+		return brandService.updateBrand(id, name, country);
+	}
+	
+	@MutationMapping
+	public Brand deleteBrand(@Argument int id){
+		return brandService.deleteBrand(id);
+	}
+	
+	// MODEL
+	
+	@QueryMapping
+	public List<Model> findAllModels(){
+		return brandService.findAllModels();
+	}
+	
+	@QueryMapping
+	public Model findOneModel(@Argument int id){
+		return brandService.findOneModel(id);
+	}
+	
+	@MutationMapping
+	public Model saveModel(@Argument int brandId, @Argument String name){
+		return brandService.saveModel(brandId, name);
+	}
+	
+	@MutationMapping
+	public Model updateModel(@Argument int id, @Argument String name){
+		return brandService.updateModel(id, name);
+	}
+	
+	@MutationMapping
+	public Model deleteModel(@Argument int id){
+		return brandService.deleteModel(id);
 	}
 }
--- graphql-backend/src/main/java/com/dkey/graphql/service/BrandService.java
+++ graphql-backend/src/main/java/com/dkey/graphql/service/BrandService.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dkey.graphql.entity.Brand;
+import com.dkey.graphql.entity.Model;
 import com.dkey.graphql.enums.Country;
 import com.dkey.graphql.repository.BrandRepository;
+import com.dkey.graphql.repository.ModelRepository;
 
 import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
@@ -15,8 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class BrandService {
+	
 	@Autowired
 	private BrandRepository brandRepository;
+	
+	@Autowired
+	private ModelRepository modelRepository;
 	
 	public List<Brand> findAllBrands(){
 		return brandRepository.findAll();
@@ -27,16 +33,67 @@ public class BrandService {
 				.orElseThrow(()->new RuntimeException("Id not exists"));
 	}
 	
-	public Brand save(String name, Country country) {
+	public Brand saveBrand(String name, Country country) {
 		Brand brand = Brand.builder().name(name).country(country).build();
 		return brandRepository.save(brand);
 	}
 	
+	public Brand updateBrand(int id, String name, Country country) {
+		Brand brand = brandRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+		brand.setName(name);
+		brand.setCountry(country);
+		return brandRepository.save(brand);
+	}
+	
+	public Brand deleteBrand(int id) {
+		Brand brand = brandRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+		
+		brandRepository.delete(brand);
+		return brand;
+	}
+	
+	public List<Model> findAllModels(){
+		return modelRepository.findAll();
+	}
+	
+	public Model findOneModel(int id) {
+		return modelRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+	}
+	
+	public Model saveModel(int brandId, String name) {
+		Brand brand = brandRepository.findById(brandId)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+		
+		return modelRepository.save(Model.builder().name(name).brand(brand).build());
+	}
+	
+	public Model updateModel(int id, String name) {
+		Model model = modelRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+		model.setName(name);
+		return modelRepository.save(model);
+	}
+	
+	public Model deleteModel(int id) {
+		Model model = modelRepository.findById(id)
+				.orElseThrow(()->new RuntimeException("Id not exists"));
+		
+		modelRepository.delete(model);
+		return model;
+	}
+	
 	@PostConstruct
 	private void loadData() {
-		save("Mers", Country.GER);
-		save("BMW", Country.GER);
-		save("Jaguar", Country.ENG);
+		saveBrand("Mers", Country.GER);
+		saveBrand("BMW", Country.GER);
+		saveBrand("Jaguar", Country.ENG);
+		
+		saveModel(1, "Benz");
+		saveModel(2, "X5");
+		saveModel(3, "F-TYPE");
 		
 	}
 }
